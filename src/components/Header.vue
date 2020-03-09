20a30bb42c6ad44e914321b80126bfe57c7f77b8
Your Name <you@example.com>
2020-03-09 19:59:18 -0300

    commit inicial

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div id=" header">
+        <header class="header">
+            <div class="header">
+                  <img src="" alt="">
+
+            </div> 
+            <div class="nav-header">
+                <nav class="nav">
+                    <div class="router-liks">
+                        <router-link to="/">Página Inicial</router-link>
+                         <router-link to="/about"></router-link>
+                          <router-link to="/"></router-link>
+                          </div>
+                </nave>
+                </div>
+            </header>
+    </div>   
+ <template>        
+
+     </script>
+         expport default
+name: "Header"
+<style scoped>
+
+header {
+    width; 100vw;
+}
+         <style>
